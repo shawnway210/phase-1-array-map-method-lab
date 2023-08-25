4de988897041331d6c7dfac722a475a78856f1ad
shawnconway <shawnway210@gmail.com>
2023-08-25 14:37:16 -0400

    map

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  {
+   name:'what does the this keyword mean?'},
+  {name:'What is the Constructor OO pattern?'},
+  {name:'implementing Blockchain Web API'},
+  {name:'The Test Driven Development Workflow'},
+  {name:'What is NaN and how Can we Check for it'},
+  {name:'What is the difference between stopPropagation and preventDefault?'},
+  {name:'Immutable State and Pure Functions'},
+  {name:'what is the difference between == and ===?'},
+  {name:'what is the difference between event capturing and bubbling?'},
+  {name:'what is JSONP?'}
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(tutorial => {
+    const words =tutorial.name.split(' ')
+    const titleCaseWords=words.map(word => {
+      const firstLetter = word.charAt(0).toUpperCase()
+      const restOfWord = word.slice(1)
+      return firstLetter + restOfWord 
+       })
+       const titleCasedName =titleCaseWords.join(' ')
+       return titleCasedName
+    })
 }
+  console.log(titleCased())
+
+  
+
